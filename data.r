3e36d32df97659a6a35811147d7cc509a4f03096
Toni <112602858+pytoni@users.noreply.github.com>
2023-06-01 10:55:49 -0500

    Add files via upload

---
+++ data.r
@@ -0,0 +1,64 @@
+projects <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "A Simple Point-of-Sale (POS) System", "SEIS 603 Python Project", 2023, "University of St. Thomas", "This Python project develops a console-based POS system, covering user authentication, inventory management, sales activities, and report generation.",
+    "FIFA World Cup Database", "SEIS 630 Database Project", 2023, "University of St. Thomas", "This project aims to design a database for the FIFA World Cup, managing data on teams, players, matches, and tournament statistics. It applies theoretical knowledge from a database management course and utilizes tools like Oracle SQL Developer, Oracle Data Modeler, and Jupyter Notebook."
+)
+
+certifications <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "IBM Data Analyst Professional Certificate", "Online", "2022", "Coursera", "This certificate equips learners with the necessary knowledge and practical skills in data analysis, including data manipulation, visualization, and reporting using tools like Excel, SQL, Python, Jupyter Notebooks, and Cognos Analytics.",
+    "Google Data Analytics Professional Certificate", "Online", "2022", "Coursera", "This ceritifcate offers individuals the opportunity to gain competency in tools and platforms such as spreadsheets, SQL, Tableau, and R, enabling them to effectively prepare, process, analyze, and share data for actionable insights.",
+    
+)
+
+skills <- tribble(
+    ~area, ~skills,
+    "Programming Languages", "Python, R, SQL",
+    "Data Visualization", "Power BI, Tableau, Google Data Studio",
+    "Others", "Analysis and research, Problem solving, Management, Teamwork and Collaboration, Flexibility"
+)
+
+honors <- tribble(
+    ~area, ~accomplishment, ~year, ~where, ~detail,
+    "Second Upper Class of Honors", "B.E in Electrical & Electronic Engineering", 2008, "Nanyang Technological Univeristy", NA
+)
+
+edu <- tribble(
+    ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+    "University of St. Thomas", 2023, 2024, "Master of Science in Data Science", "Minnesota, U.S.A",NA,
+    "Nanyang Technological University", 2008, 2010, "Master of Science in Electronics", "Singapore",NA,
+    "Nanyang Technological University", 2004, 2008, "B.E in Electrical & Electronic Engineering", "Singapore", NA
+    
+)
+
+experience <- tribble(
+    ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+    "Deputy Manager of Technology - Quality Department", "Hoa Sen Group", "April", 2019, "March", 2022, "Ho Chi Minh City - Vietnam", "Orchestrated quality performance management for ten factories, delivering a notable 5% enhancement in 
+the product quality index during FY 2020-2021.",
+    "Deputy Manager of Technology - Quality Department", "Hoa Sen Group", "April", 2019, "March", 2022, "Ho Chi Minh City - Vietnam", "Swiftly resolved customer complaints, leading to substantial annual savings of approximately US$2,000,000 
+for the company.",
+    "Deputy Manager of Technology - Quality Department", "Hoa Sen Group", "April", 2019, "March", 2022, "Ho Chi Minh City - Vietnam", "Proactively provided actionable insights and recommendations to the Board of Directors through effective 
+data analysis and Power BI dashboard reports, contributing to improvements in customer service, product quality, and reliability.",
+    "Director of Quality Management Department", "Hoa Sen Group", "October", 2015, "March", 2019, "Ho Chi Minh City - Vietnam", "Recognized and rapidly promoted for expertise in managing quality systems and handling customer 
+complaints, employing data analysis techniques.",
+    "Director of Quality Management Department", "Hoa Sen Group", "October", 2015, "March", 2019, "Ho Chi Minh City - Vietnam", "Developed and implemented innovative methods and procedures using Power BI, Google Sheets, and Google 
+Data Studio to collect, track, and analyze production data and customer complaint records.",
+    "Director of Quality Management Department", "Hoa Sen Group", "October", 2015, "March", 2019, "Ho Chi Minh City - Vietnam", "Utilized advanced statistical analysis and data visualization techniques to identify areas for process 
+optimization, driving improvements in operational efficiency and overall quality management.",
+    "Head of Quality Management Department", "Hoa Sen Group", "July", 2015, "September", 2015, "Ho Chi Minh City - Vietnam", "Leveraged data analysis techniques to monitor and analyze production logs and defects data on a weekly and 
+monthly basis, providing valuable findings and insights to the factories for product and quality enhancement.",
+    "Head of Quality Management Department", "Hoa Sen Group", "July", 2015, "September", 2015, "Ho Chi Minh City - Vietnam", "Successfully planned, implemented, and advanced quality systems, standards, and specifications across all 
+levels of the organization, utilizing data-driven insights to drive continuous improvement initiatives.",
+    "Quality Executive", "Hoa Sen Group", "August", 2014, "June", 2015, "Ho Chi Minh City - Vietnam", "Collaborated with factory Quality Control teams to collect, validate, and report production data to internal stakeholders 
+(e.g, Trade Remedies, Sales, Purchasing, and Supply Chain), providing valuable insights for business requirements.",
+    "Quality Executive", "Hoa Sen Group", "August", 2014, "June", 2015, "Ho Chi Minh City - Vietnam", "Prepared comprehensive product and quality reports using advanced analytical methods, collecting, 
+analyzing, and summarizing data to facilitate data-driven decision-making for the Quality Management Department Director.",
+    "Quality Executive", "Hoa Sen Group", "August", 2014, "June", 2015, "Ho Chi Minh City - Vietnam", "Played a key role in reviewing and updating international standards and specifications, ensuring their 
+effective implementation across the organization.",
+    "Project Officer and Research Associate", "Nanyang Technological University", "September", 2008, "February", 2013, "Singapore", "Conducted research on multiple exciton generation in Silicon-nanowires and nanocrystals for solar cell 
+applications.",
+    "Project Officer and Research Associate", "Nanyang Technological University", "September", 2008, "February", 2013, "Singapore", "In-depth researched and developed PEDOT:PSS based supercapacitors with high capacitance.",
+    "Project Officer and Research Associate", "Nanyang Technological University", "September", 2008, "February", 2013, "Singapore", "Developed the fabrication process of waveguides using Sol-gel and Laser techniques."
+)
+
+
